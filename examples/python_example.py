1e876894ca20f568f019c3eb585e5615d8c0a1cb
Abhijeet Kumar <abhijeet151@gmail.com>
2025-02-22 19:58:38 +0530

    Update python_example.py

--- examples/python_example.py
+++ examples/python_example.py
@@ -1,1 +1,14 @@
+from super_init import super_init
+
+class Parent:
+    def __init__(self):
+        print("Parent __init__ called")
+
+@super_init
+class Child(Parent):
+    def __init__(self):
+        print("Child __init__ called")
+
+child1 = Child()  # Parent's __init__ is not called
+child2 = Child(_use_super=True)  # Parent's __init__ is called
 
